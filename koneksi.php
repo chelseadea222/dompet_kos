6214123aebcbc590ff471bf312bf08cca8e8acca
chelseadea222 <chelseadea222@gmail.com>
2026-05-23 22:37:32 +0700

    baru

--- koneksi.php
+++ koneksi.php
@@ -3,16 +3,24 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// UBAH DENGAN KREDENSIAL DATABASE ONLINE ANDA
 $host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com"; 
 $user = "48D8BJbofbESMRv.root";
 $pass = "0aaL5V7vTVUnnPtZ";
 $db   = "dompetkos"; 
-$port = "4000";
+$port = 4000;
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+// 1. Inisialisasi koneksi MySQLi
+$conn = mysqli_init();
 
-if ($conn->connect_error) {
-    die("Koneksi database gagal: " . $conn->connect_error);
+// 2. Beritahu PHP untuk menggunakan SSL (Wajib untuk TiDB)
+$conn->ssl_set(NULL, NULL, NULL, NULL, NULL);
+
+// 3. Lakukan koneksi sebenarnya dengan flag MYSQLI_CLIENT_SSL
+$berhasil = $conn->real_connect($host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+
+// Cek apakah koneksi berhasil
+if (!$berhasil) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
 }
+
 ?>
