dd4158813f05a070264a1c034e97e5c4884e220f
chelseadea222 <chelseadea222@gmail.com>
2026-05-23 21:52:31 +0700

    baru

--- koneksi.php
+++ koneksi.php
@@ -3,15 +3,16 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "dompetkos";
+// UBAH DENGAN KREDENSIAL DATABASE ONLINE ANDA
+$host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com"; 
+$user = "48D8BJbofbESMRv.root";
+$pass = "0aaL5V7vTVUnnPtZ";
+$db   = "dompetkos"; 
+$port = "4000";
 
-$conn = new mysqli($host, $user, $pass, $db);
-$conn->set_charset("utf8mb4");
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+    die("Koneksi database gagal: " . $conn->connect_error);
 }
 ?>
